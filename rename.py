58df8b5ea497ecec25ee052e820526bc1fe81dc3
William Wu <w@qed.ai>
2014-10-07 11:46:13 +0300

    implemented suffix substitution with integer increment

--- rename.py
+++ rename.py
@@ -2,12 +2,17 @@
 # usage: ./renamer [data_directory]
 
 import os, re
+from collections import defaultdict
 
 data_dir = "test" 
 
+tally = defaultdict(int)
 files = sorted(os.listdir(data_dir))
 for a in files: 
     b = re.sub('_','',a, 1) 
     c = re.sub('_','.',b,1) 
-    print "{0} ==> {1}".format(a,c) 
-
+    prefix = c.split('.')[0]
+    tally[prefix] += 1 
+    d =  prefix + "." + str(tally[prefix]-1)
+    print "{0} ==> {1}".format(a,d) 
+    os.rename( os.path.join(data_dir, a), os.path.join(data_dir,  d))
